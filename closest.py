9d9cae5ef773318ec33c908e0fd05b7c0bd1be55
connorshil <31968006+connorshil@users.noreply.github.com>
2017-12-05 09:45:28 -0700

    Add files via upload

---
+++ closest.py
@@ -0,0 +1,14 @@
+def brute_force(x,y):
+   return 0
+
+def middle_helper(y,delta,midpoint):
+    return 0
+
+def calcDist(x,y):
+    return 0
+
+def compute_closest_points():
+    return 0
+
+if __name__ == '__main__':
+    compute_closest_points()
